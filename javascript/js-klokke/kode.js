5bca9651f4853fc84ed6b1670a93aa6b44ac5112
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-10-24 16:36:52 +0200

    Klokke i JS, visning vha HTML og CSS

---
+++ javascript/js-klokke/kode.js
@@ -0,0 +1,34 @@
+/*
+Vi ser først på hva dette resulterer i, i fellesskap:
+let tid = new Date();
+document.getElementById("utskrift").innerText = tid; 
+
+Oppgave 1:
+ - Ein annan formatering på utskrifta. 
+   Hint: getFullYear() ... fra når me rekna ut alderen når brukeren skreiv inn fødselsår
+
+Oppgave 2: 
+ - Få klokka til å auto-oppdatere seg kvart sekund.
+   Hint: intervall og W3Schools
+
+*/
+
+// Oppgåve 1 - eksperimentering
+//let tid = new Date();
+//document.getElementById("utskrift").innerText = tid; 
+
+// console.log(tid.getHours());
+// console.log(tid.getMinutes());
+// console.log(tid.getSeconds());
+
+// console.log(tid.getHours() + " h : " + tid.getMinutes() + " m : " + tid.getSeconds() + " s");
+
+// Oppgåve 2 - å få all koden over til å køyre automatisk kvart sekund
+// Kilde: https://www.w3schools.com/js/js_timing.asp
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+    let tid = new Date();
+    document.getElementById("utskrift").innerText = tid.getHours() + " h : " + tid.getMinutes() + " m : " + tid.getSeconds() + " s";
+}

7dab6ad2db52472607ebadfd94268b3be20a57b3
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-10-27 12:06:49 +0100

    Avansert biletgalleri

--- javascript/js-bildegalleri-bildetekst/kode.js
+++ javascript/js-bildegalleri-bildetekst/kode.js
@@ -1,33 +1,55 @@
-let bilde = document.getElementById("bilde");
+// let bilde = document.getElementById("bilde");
 const bildetekst = document.getElementById("bildetekst");
 
-// bilde.src = "bilder/bilde_fjell.jpg";
-
 let bildeGalleriMedTekst = [
-    { "bilde" : "bilde_bøker.jpg", "bildeTekst" : "Et flott bilde av bøker." },
-    { "bilde" : "bilde_fjell.jpg", "bildeTekst" : "Fjell FTW!" },
-    { "bilde" : "bilde_foss.jpg",  "bildeTekst" : "Foss FTW!" },
-    { "bilde" : "bilde_frosk.jpg", "bildeTekst" : "Frosk FTW!" },
-    { "bilde" : "bilde_hest.jpg",  "bildeTekst" : "Hest FTW!" }
+    { "bilde" : "granvin.jpg",     "bildeTekst" : "Granvin, utkikspunktet" },
+    { "bilde" : "ingebjørgnuten.jpg", "bildeTekst" : "Vårski på Ingebjørgnuten" },
+    { "bilde" : "midtfjell.jpg",  "bildeTekst" : "Elisabeth på Midtfjell" },
+    { "bilde" : "nesheimshorgi.jpg", "bildeTekst" : "Nesheimshorgi, frå Haugse" },
+    { "bilde" : "olastøl_skåndalshorgi.jpg",  "bildeTekst" : "Påskeski på Olastøl" },
+    { "bilde" : "olastøl_sommar.jpg", "bildeTekst" : "Olastøl, frå Nesheimshorgi" },
+    { "bilde" : "skiglede.jpg", "bildeTekst" : "Ski gjer, og ski tar" },
+    { "bilde" : "ulriken.jpg", "bildeTekst" : "Bergen, frå Ulriken" },
+    { "bilde" : "vetlahorgi.jpg", "bildeTekst" : "Telttur på Vetlahorgi" },
 ];
 
-console.log(bildeGalleriMedTekst[0].bilde);
-console.log(bildeGalleriMedTekst[0].bildeTekst);
-
-// bilde.src = "bilder/" + bildeGalleri[1];
-
+// Byrjar med at det første biletet i arrayen vises
 let aktivtBilde = 0;
-bilde.src = "bilder/" + bildeGalleriMedTekst[aktivtBilde].bilde;
 
-setInterval(skiftBilde, 1000);
+// Gamal løysing, der me viser fram eit bilete vha img-taggen
+// bilde.src = "bilder/" + bildeGalleriMedTekst[aktivtBilde].bilde;
 
+// Ny løysing, der biletet er bakgrunnen på heile nettsida
+document.body.style.backgroundImage = `url(bilder/${bildeGalleriMedTekst[aktivtBilde].bilde})`;
+
+// Køyrer skiftBilde-funksjonen så ofte som tidsintervallet tilseier
+tidsintervall = 5000;
+let endring = setInterval(skiftBilde, tidsintervall);
+
+// Funksjonen som faktisk byter biletet
 function skiftBilde() {
     aktivtBilde = aktivtBilde + 1;
     
-    if (aktivtBilde > bildeGalleriMedTekst.length-1) { // Sjekker om jeg kommer utenfor arrayen, altså ikke noe bilde
+    if (aktivtBilde >= bildeGalleriMedTekst.length) { // Sjekker om jeg kommer utenfor arrayen, altså ikke noe bilde
         aktivtBilde = 0;
     }
     
-    bilde.src = "bilder/" + bildeGalleriMedTekst[aktivtBilde].bilde;
+    // Ny versjon
+    document.body.style.backgroundImage = `url(bilder/${bildeGalleriMedTekst[aktivtBilde].bilde})`;
     bildetekst.innerText = bildeGalleriMedTekst[aktivtBilde].bildeTekst;
+    
+    // Gamal versjon
+    // bilde.src = "bilder/" + bildeGalleriMedTekst[aktivtBilde].bilde;
 }
+
+// Gjer det mogleg å endre innstillingane
+document.addEventListener("keydown", (event) => {
+    if (event.key === "i" || event.key === "I") {
+        let nyttTidsintervall = prompt("Angi nytt tidsintervall i millisekunder:", tidsintervall);
+        if (nyttTidsintervall !== null) {
+            tidsintervall = parseInt(nyttTidsintervall);
+            clearInterval(endring);
+            intervalId = setInterval(skiftBilde, tidsintervall);
+        }
+    }
+});

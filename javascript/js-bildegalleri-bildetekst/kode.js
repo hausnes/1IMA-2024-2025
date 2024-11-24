c9a42754aa6c0e9dcf3677ef4798a8c74c33f315
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-11-24 20:32:14 +0100

    Ryddigare array med objekt

--- javascript/js-bildegalleri-bildetekst/kode.js
+++ javascript/js-bildegalleri-bildetekst/kode.js
@@ -2,15 +2,15 @@
 const bildetekst = document.getElementById("bildetekst");
 
 let bildeGalleriMedTekst = [
-    { "bilde" : "granvin.jpg",     "bildeTekst" : "Granvin, utkikspunktet" },
-    { "bilde" : "ingebjørgnuten.jpg", "bildeTekst" : "Vårski på Ingebjørgnuten" },
-    { "bilde" : "midtfjell.jpg",  "bildeTekst" : "Elisabeth på Midtfjell" },
-    { "bilde" : "nesheimshorgi.jpg", "bildeTekst" : "Nesheimshorgi, frå Haugse" },
+    { "bilde" : "granvin.jpg",                "bildeTekst" : "Granvin, utkikspunktet" },
+    { "bilde" : "ingebjørgnuten.jpg",         "bildeTekst" : "Vårski på Ingebjørgnuten" },
+    { "bilde" : "midtfjell.jpg",              "bildeTekst" : "Elisabeth på Midtfjell" },
+    { "bilde" : "nesheimshorgi.jpg",          "bildeTekst" : "Nesheimshorgi, frå Haugse" },
     { "bilde" : "olastøl_skåndalshorgi.jpg",  "bildeTekst" : "Påskeski på Olastøl" },
-    { "bilde" : "olastøl_sommar.jpg", "bildeTekst" : "Olastøl, frå Nesheimshorgi" },
-    { "bilde" : "skiglede.jpg", "bildeTekst" : "Ski gjer, og ski tar" },
-    { "bilde" : "ulriken.jpg", "bildeTekst" : "Bergen, frå Ulriken" },
-    { "bilde" : "vetlahorgi.jpg", "bildeTekst" : "Telttur på Vetlahorgi" },
+    { "bilde" : "olastøl_sommar.jpg",         "bildeTekst" : "Olastøl, frå Nesheimshorgi" },
+    { "bilde" : "skiglede.jpg",               "bildeTekst" : "Ski gjer, og ski tar" },
+    { "bilde" : "ulriken.jpg",                "bildeTekst" : "Bergen, frå Ulriken" },
+    { "bilde" : "vetlahorgi.jpg",             "bildeTekst" : "Telttur på Vetlahorgi" }
 ];
 
 // Byrjar med at det første biletet i arrayen vises

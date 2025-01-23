32a5490771c4b689ba6c73d662c8e775ad2543a1
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-01-23 21:54:17 +0100

    Starteksempel kryptering

---
+++ sikkerheit/kryptering-basic/kode.js
@@ -0,0 +1,16 @@
+let alfabet = "0123456789abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ,.-_!?";
+let alfabetLengde = alfabet.length;
+console.log("Alfabetet sin lengde: " + alfabetLengde);
+
+// Krypteringsfunksjonen, som får inn bokstaven ein skal kryptere, samt krypteringsnøkkelen. 
+// Returnerer den nye bokstaven.
+function krypterBokstav(bokstavInn, krypteringsnokkelInn) {    
+    
+    // Her skal magien skje! Skriv din eigen kode.
+
+}
+
+console.log(alfabet.indexOf("0")); // Skal returnere 0
+console.log(alfabet.indexOf("a")); // Skal returnere 10
+
+console.log(krypterBokstav("a", 1)); // Skal returnere "b"

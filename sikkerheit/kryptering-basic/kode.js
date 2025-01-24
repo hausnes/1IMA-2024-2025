76426d8eccac7bb7a3da6271999c9998350f7b96
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-01-24 12:02:36 +0100

    Krypteringseksempel frå timen

--- sikkerheit/kryptering-basic/kode.js
+++ sikkerheit/kryptering-basic/kode.js
@@ -1,16 +1,52 @@
-let alfabet = "0123456789abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ,.-_!?";
+let alfabet = "0123456789abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ,.-_!? ";
 let alfabetLengde = alfabet.length;
 console.log("Alfabetet sin lengde: " + alfabetLengde);
 
 // Krypteringsfunksjonen, som får inn bokstaven ein skal kryptere, samt krypteringsnøkkelen. 
 // Returnerer den nye bokstaven.
-function krypterBokstav(bokstavInn, krypteringsnokkelInn) {    
+function krypterBokstav(bokstavInn, krypteringsnokkelInn) {  
+    let posisjon   = alfabet.indexOf(bokstavInn);
+    let posisjonNy = posisjon + krypteringsnokkelInn;
     
-    // Her skal magien skje! Skriv din eigen kode.
-
+    if (posisjonNy >= alfabetLengde) {
+        posisjonNy = posisjonNy - alfabetLengde;
+    }
+    
+    return alfabet[posisjonNy];
 }
 
-console.log(alfabet.indexOf("0")); // Skal returnere 0
-console.log(alfabet.indexOf("a")); // Skal returnere 10
-
 console.log(krypterBokstav("a", 1)); // Skal returnere "b"
+
+function dekrypterBokstav(bokstavInn, krypteringsnokkelInn) {  
+    let posisjon   = alfabet.indexOf(bokstavInn);
+    let posisjonNy = posisjon - krypteringsnokkelInn;
+    
+    if (posisjonNy < 0) {
+        posisjonNy = posisjonNy + alfabetLengde;
+    }
+    
+    return alfabet[posisjonNy];
+}
+
+console.log(dekrypterBokstav("b", 1));
+
+// Her tester vi om det går an å kryptere en hel setning
+
+let setning = "Angrip i morgen, klokka 08.15!";
+let kryptertSetning = "";
+
+for (let bokstav of setning) {
+    kryptertSetning = kryptertSetning + krypterBokstav(bokstav, 1);
+}
+
+console.log(kryptertSetning); 
+
+let hemmeligBeskjed = "Yvzb vx yvzHvF ÆC SæPFBrFc Vx urBGrF HtyrdHtyrdHtyr øJrF øJvåu wPF vx åvxx Avxc";
+let nokkel = 17;
+let avsloring = "";
+
+for (let bokstav of hemmeligBeskjed) {
+    avsloring = avsloring + dekrypterBokstav(bokstav, nokkel);
+}
+
+console.log(avsloring);

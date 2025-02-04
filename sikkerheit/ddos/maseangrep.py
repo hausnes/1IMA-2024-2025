3e8559a3a69465eb50e51894cc8bd05ec8fb1f69
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-02-04 12:36:24 +0100

    Konsept bak DDoS - ver varsam

---
+++ sikkerheit/ddos/maseangrep.py
@@ -0,0 +1,41 @@
+'''
+Dette programmet sender førespurnader til ein server i ei løkke, og nyttar trådar om du ynskjer. 
+Programmet kan brukast til å teste korleis ein server handterer mange førespurnader samtidig. 
+For å teste programmet, må du ha ein server som lyttar på port 3000.
+Ver varsam med å bruke dette programmet, då det kan føre til at serveren blir overbelasta og sluttar å fungere.
+
+---------------------------------------------------------------------------------------
+Du skal ikkje bruke dette programmet til å sende førespurnader til ein server utan lov.
+---------------------------------------------------------------------------------------
+
+'''
+
+import threading
+import requests
+import time
+
+# Flaggvariabel for å kontrollere avslutning av trådane
+stop_threads = False
+
+def send_request():
+    while not stop_threads:
+        response = requests.get('http://localhost:3000')
+        print(response.status_code)
+        time.sleep(1)  # Sov i 1 sekund mellom kvar førespurnad
+
+# Opprett og start trådar
+threads = []
+for i in range(1):  # Antall trådar som sender førespurnader
+    thread = threading.Thread(target=send_request)
+    thread.start()
+    threads.append(thread)
+
+# Vent på at brukaren skal trykke på Enter for å avslutte
+input("Trykk Enter for å avslutte...\n")
+stop_threads = True
+
+# Vent på at alle trådane skal avslutte
+for thread in threads:
+    thread.join()
+
+print("Programmet er avslutta.")

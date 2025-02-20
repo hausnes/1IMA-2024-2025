a353b06bf3393e6e24d3a14b8f11cd3f505155c2
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-02-20 23:35:58 +0100

    Moglegheit for å legge til bilar via nettside

---
+++ databaser/bildatabase/app05.js
@@ -0,0 +1,44 @@
+// Video med forklaring: Kjem...
+/*
+ 
+*/
+
+const express = require("express");
+const app = express();
+
+const Database = require("better-sqlite3");
+const db = new Database("bil.db");
+
+const PORT = 3000;
+
+// Middleware for å servere statiske filer fra public-mappen
+app.use(express.static('public'));
+
+// Middleware for å parse JSON-data
+app.use(express.json());
+
+// Eksempel på rute som hentar brukarar frå databasen (besøk http://localhost:3000/personer)
+app.get("/personer", (req, res) => {
+    const users = db.prepare("SELECT * FROM person").all();
+    res.json(users);
+});
+
+// Eksempel på rute som hentar bilar frå databasen (besøk http://localhost:3000/biler)
+app.get("/biler", (req, res) => {
+    const cars = db.prepare("SELECT * FROM bil").all();
+    res.json(cars);
+});
+
+// Rute for å legge til ein ny bil i databasen
+app.post("/leggtilbil", (req, res) => {
+    const { registreringsnummer, merke, modell, personnummer } = req.body;
+
+    const stmt = db.prepare("INSERT INTO bil (personnummer, registreringsnummer, merke, modell) VALUES (?, ?, ?, ?)");
+    const info = stmt.run(personnummer, registreringsnummer, merke, modell);
+
+    res.json({ message: "Ny bil lagt til", info });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server køyrer: http://localhost:${PORT}`);
+});
